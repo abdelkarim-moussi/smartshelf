0b14f630e571ee3722a18cf436c18ecb67a4af57
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-03-11 22:20:32 +0000

    user login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -21,11 +22,35 @@ class AuthController extends Controller
             );
 
         $user = User::create($fields);
-        $token = $user->createToken($user->name);
+        $token = $user->createToken($request->name);
 
         return [
             'user'=>$user,
             'token'=>$token->palinTextToken
         ];
+    }
+
+    public function login(Request $request){
+
+        $fields = $request->validate([
+            'email'=>'required|exists:users',
+            'password'=>'required'
+        ]);
+
+        $user = User::where('email',$request->email)->first();
+
+        if(!$user || !Hash::check($request->password,$user->password)){
+            return [
+                'message'=>"incorrect credentials"
+            ];
+        }
+
+        $token = $user->createToken($user->name);
+
+        return [
+            'user'=>$user,
+            'token'=>$token->plainTextToken
+        ];
+
     }
 }
